3eb1b75c1a83dc9d6533b05e3a76552d5f563cc8
priya <priya123@gmail.com>
2024-12-23 15:21:37 +0530

    sg_tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,22 @@
 provider "aws"{
     region="us-east-1"
 }
+resouce "aws_security_group""my_sg" {
+    name= "my_sg"
+    description= "ssh"
+    ingress {
+        from_port  = 22 
+        to_port = 22
+        protocol = "SSH"
+        cidr_blocks =["0.0.0.0/0"]
+    }
+    egress {
+        from_port = 22
+        to_port = 22
+        protocol = "SSH"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
 resource "aws_instance" "my-instance" {
   ami=var.image_id
   instance_type= "t2-micro"
