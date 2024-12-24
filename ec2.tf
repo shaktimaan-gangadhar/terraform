7a0c9734d72ac37ff4051ce9a45343c3efef2f35
priya <priya123@gmail.com>
2024-12-24 16:02:46 +0530

    refine-commit

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 provider "aws"{
-    region="us-east-1"
+    region=""
 }
 resouce "aws_security_group""my_sg" {
     name= "my_sg"
@@ -20,12 +20,12 @@ resouce "aws_security_group""my_sg" {
 resource "aws_instance" "my-instance" {
   ami=var.image_id
   instance_type= "t2.micro"
-  key_name= "hello"
-  vpc_security_groups_id=[aws_security_group.my_sg.id]
+  key_name= ""
+  vpc_security_group_ids=[aws_security_group.my_sg.id]
 }
 variable "image_id" {
     type= string
-    default="ami-01816d07b1128cd2d"
+    default=""
     description="Enter_my_value"
 }
 output "my_ip"{
